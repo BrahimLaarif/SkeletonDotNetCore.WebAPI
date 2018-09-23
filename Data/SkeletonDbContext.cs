fbe784bc86f72247c027b0b231fb6c971bd77454
Brahim Laarif <brahim.laarif@gmail.com>
2018-09-23 09:29:29 +0100

    Add values migration

--- Data/SkeletonDbContext.cs
+++ Data/SkeletonDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using SkeletonDotNetCore.WebAPI.Models;
 
 namespace SkeletonDotNetCore.WebAPI.Data
 {
@@ -8,5 +9,7 @@ namespace SkeletonDotNetCore.WebAPI.Data
             : base(options)
         {
         }
+
+        public DbSet<Value> Values { get; set; }
     }
 }
